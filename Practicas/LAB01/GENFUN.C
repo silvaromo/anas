e2f62225f23fc6c24c5d7b1618d135fb8ce8c0cc
Ana Sofia <anasofia.silvaromo@gmail.com>
2020-10-10 00:17:34 -0500

    ASCII

--- Practicas/LAB01/GENFUN.C
+++ Practicas/LAB01/GENFUN.C
@@ -3,6 +3,26 @@
 
 void main (void)
 {
-	unit jltest[12]="";
-	printf("");
+	uint8 au8BufferTest[12]= "hoLA nenA BB";
+	printf("before function %s\n", au8BufferTest);
+	GENFUN_vCapsOff(&au8BufferTest[0], 12);
+	printf("after function %s\n", au8BufferTest);
+	
+}
+Void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList)
+{
+	while (u8SizeOfList !=0)
+	{
+		if( *pu8Src=> ASCII_LOW_THRESHOLD_CAPSON_TO_OFF &&
+		    *pu8Src <= ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF)
+		{
+			*pu8Scr += ASCII_CONVERTION_FACTOR
+		}
+		else
+		{
+			/*nothing to do*/
+		}
+		pu8Src++;
+		u8SizeOfList --;
+	}
 }
---
+++ Practicas/LAB01/GENFUN.h
@@ -0,0 +1,13 @@
+/*prototypes*/
+Void GENFUN_vCapsOff (uint8 *pu8Src,uint8 u8SizeOfList);
+
+/*definitions*/
+
+#define ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF	90
+#define ASCII_LOW_THRESHOLD_CAPSON_TO_OFF	65
+#define ASCII_CONVERTION_FACTOR			32
+
+/*Data Types*/
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
